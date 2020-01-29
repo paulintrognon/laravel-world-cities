973350e6284eb03e349d33ac218fa023bd23633c
Paulin Trognon <paulin.trognon@gmail.com>
2020-01-29 10:08:09 +0100

    add select2 plugin

--- src/Models/LwcCities.php
+++ src/Models/LwcCities.php
@@ -21,4 +21,37 @@ class LwcCity extends Model
     {
         return $this->hasMany('PaulinTrognon\LaravelWorldCities\Models\LwcCityAlternateName');
     }
+
+    // Helpers
+
+    /**
+     * For Select2 plugin
+     * https://select2.org/data-sources/ajax
+     */
+    public static function select2($searchText, array $params = [])
+    {
+        $countryIso2 = $params['countryIso2'] ?? null;
+
+        $citiesQuery = self::wherehas('alternateNames', function ($query) use ($searchText) {
+            $query->where('name', 'LIKE', "$searchText%");
+        });
+
+        if ($countryIso2 !== null) {
+            $citiesQuery = $citiesQuery->where('country_iso2', $countryIso2);
+        }
+
+        $cities = $citiesQuery
+            ->orderBy('name')
+            ->take(100)
+            ->get();
+
+        $select = [];
+        foreach ($cities as $city) {
+            $select[] = [
+                "id" => $city->id,
+                "text" => "$city->name ($city->adm2)",
+            ];
+        }
+        return $select;
+    }
 }
